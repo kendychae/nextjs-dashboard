f951237476ddc12bcc07632fd2a66ccd84be1f11
kendychae@gmail.com <kendychae@gmail.com>
2025-10-29 13:06:57 -0600

    Temporarily disable authentication middleware for assignment submission

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  // Temporarily disable middleware for assignment submission
+  // matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [],
 };
